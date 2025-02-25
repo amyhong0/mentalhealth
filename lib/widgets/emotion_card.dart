c813f2282083ea7da4e0342915a209ed0db50461
Hyewon <77429415+amyhong0@users.noreply.github.com>
2025-02-25 11:29:11 +0900

    Update emotion_card.dart

--- lib/widgets/emotion_card.dart
+++ lib/widgets/emotion_card.dart
@@ -38,3 +38,42 @@ class EmotionCard extends StatelessWidget {
           ],
         ),
       ),
+      back: _buildCardFace(
+        color: emotion.category == '긍정' ? Colors.green[100]! : Colors.red[100]!,
+        child: Center(
+          child: Text(
+            emotion.name,
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: emotion.category == '긍정' ? Colors.green[800] : Colors.red[800],
+            ),
+          ),
+        ),
+      ),
+      onFlip: onSelect,
+      isFront: !isSelected,
+    );
+  }
+
+  Widget _buildCardFace({required Color color, required Widget child}) {
+    return Container(
+      width: 150,
+      height: 200,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 5,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.all(16),
+      child: child,
+    );
+  }
+}
